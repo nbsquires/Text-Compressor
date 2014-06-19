28c346efd7f7a9245f25a62227d44a1e2f246a1d
nbsquires <nsquires@upei.ca>
2014-06-19 13:48:57 -0300

    Create CompressionDriver.java

---
+++ CompressionDriver.java
@@ -0,0 +1,21 @@
+/*
+ * CompressionDriver.java
+ * Author: Natasha Squires
+ * C Sc 361 Assignment #6
+ */
+import java.io.*;
+import java.util.*;
+
+public class CompressionDriver {
+
+	public static void main(String[] args) throws IOException{		
+		Scanner fileScan = new Scanner(new File("bin/test.txt"));
+		StringBuilder textToEncode = new StringBuilder();
+		while(fileScan.hasNext())
+			textToEncode.append(fileScan.nextLine().toLowerCase()+"\n");
+		
+		HuffmanEndcoding encode = new HuffmanEndcoding(textToEncode.toString());
+		encode.compression();
+	}
+
+}
